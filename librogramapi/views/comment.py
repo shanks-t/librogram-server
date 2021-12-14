d59388b2f7950f091fa1796022d1dea78a81b0f2
trey shanks <shanksleslietrey@gmail.com>
2021-12-14 14:50:04 -0600

    add and edit comments working

--- librogramapi/views/comment.py
+++ librogramapi/views/comment.py
@@ -37,8 +37,8 @@ class CommentView(ViewSet):
 
     def update(self, request, pk=None):
 
-        user = User.objects.get(user=request.auth.user)
-        book = Book.objects.get(id=request.data['postId'])
+        user = User.objects.get(username=request.auth.user)
+        book = Book.objects.get(id=request.data['bookId'])
 
         # Do mostly the same thing as POST, but instead of
         # creating a new instance of Game, get the game record
