9cfb9e97c74e0427848f5d00ec06aafc4e087c31
trey shanks <shanksleslietrey@gmail.com>
2021-12-15 13:52:41 -0600

    added query params to list for userbooks

--- librogramapi/views/user_book.py
+++ librogramapi/views/user_book.py
@@ -8,6 +8,7 @@ from rest_framework.decorators import action
 from django.http import HttpResponseServerError
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.db.models import Q
 
 from librogramapi.models import Book, Reader, UserBook, Status
 from librogramapi.models.comment import Comment
@@ -38,8 +39,18 @@ class UserBookView(ViewSet):
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request):
+
         user = request.auth.user
         user_books = UserBook.objects.filter(user=user)
+
+        search_term = self.request.query_params.get('q', None)
+
+        if search_term is not None:
+            user_books = UserBook.objects.filter(
+                Q(book__title__icontains=search_term) |
+                Q(book__author__icontains=search_term) 
+            )
+
         serializer = UserBookSerializer(
             user_books, many=True, context={'request': request}
         )
@@ -89,23 +100,24 @@ class UserBookView(ViewSet):
 
         except UserBook.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-        
+
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'username', 'is_staff' )
+        fields = ('id', 'first_name', 'last_name', 'username', 'is_staff')
+
 
 class StatusSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Status
         fields = '__all__'
+
 
 class CommentSerializer(serializers.ModelSerializer):
 
@@ -117,9 +129,11 @@ class CommentSerializer(serializers.ModelSerializer):
 class BookSerializer(serializers.ModelSerializer):
 
     comments = CommentSerializer(many=True)
+
     class Meta:
         model = Book
-        fields = ('id', 'title', 'subtitle', 'author', 'image_path', 'description', 'page_count', 'publisher', 'date_published', 'checkout_date', 'tags', 'comments')
+        fields = ('id', 'title', 'subtitle', 'author', 'image_path', 'description',
+                  'page_count', 'publisher', 'date_published', 'checkout_date', 'tags', 'comments')
 
 
 class UserBookSerializer(serializers.ModelSerializer):
@@ -127,6 +141,8 @@ class UserBookSerializer(serializers.ModelSerializer):
     status = StatusSerializer()
     book = BookSerializer()
     user = UserSerializer()
+
     class Meta:
         model = UserBook
-        fields = ('id', 'user', 'rating', 'review', 'start_date', 'finish_date', 'current_page', 'status', 'book')
+        fields = ('id', 'user', 'rating', 'review', 'start_date',
+                  'finish_date', 'current_page', 'status', 'book')
