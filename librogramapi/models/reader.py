34000f7067d120857866492495a66ee1f91568bf
ts <shanksleslietrey@gmail.com>
2022-01-17 16:25:36 -0600

    altered background image prop on reader model

--- librogramapi/models/reader.py
+++ librogramapi/models/reader.py
@@ -10,7 +10,7 @@ class Reader(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=70)
     profile_image_url = models.URLField()
-    background_image_url = models.URLField(default='', null=True)
+    background_image_url = models.URLField(default='')
     subscriber = models.BooleanField(default=False)
 
 
